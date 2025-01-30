316dd6430f5608e08fdd998f0be375799bf2f2cb
Midhun <midhun122015@gmail.com>
2025-01-30 22:03:04 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Sustain X : Building Tomorrow',
+  description: 'An initiate by IGBCCUSAT to promote sustainable living, Visit us at www.igbccusat.com',
 }
 
 export default function RootLayout({
